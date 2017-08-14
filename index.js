6f03478f09d7bfd0e3272c367237a658e5a5f979
Andrew Gibson <andrewgib14@gmail.com>
2017-08-14 14:42:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk");
+const moment = require("moment");
+
+let m = moment();
+
+console.log("It is " + m.format('dddd, MMMM Do YYYY, hh:mm:ss a.'));
+console.log("It is " + m.format('DDDD') + "th day of the year.");
+console.log("It is " + m.format);
+// console.log();
+// console.log();
+
