5dc52334b092e76c8ae5cb09e76e60037f3ac5c1
Andrew Gibson <andrewgib14@gmail.com>
2017-08-17 15:13:01 -0500

    chalked the console.log's

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const chalk = require("chalk");
 const moment = require("moment");
 
 let m = moment();
+let secondsOfDay = (m.hour() * 3600) + (m.minute() * 60) + (m.second());
 
-console.log("It is " + m.format('dddd, MMMM Do YYYY, hh:mm:ss a.'));
-console.log("It is " + m.format('DDDD') + "th day of the year.");
-console.log("It is " + m.format);
-// console.log();
-// console.log();
+console.log("It is " + chalk.blueBright(m.format('dddd, MMMM Do YYYY, hh:mm:ss a.')));
+console.log("It is " + chalk.magenta(m.format('DDDD') + "th ") + "day of the year.");
+console.log("It is " + chalk.cyan(secondsOfDay) + " seconds into the day.");
+if (m.isDST()) {
+    console.log("It " + chalk.green("is") + " Daylight Savings Time.");
+}
+else {
+    console.log("It is not Daylight Savings Time.");
+}
+if (m.isLeapYear()) {
+    console.log("It is a Leap Year.");
+}
+else {
+    console.log("It " + chalk.red("is not") + " a Leap Year.")
+}
+
+
+
 
